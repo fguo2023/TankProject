83d5b2ea39fb541d09d03d227d175e31a6b15ad6
fguo2023 <fguo520@gmail.com>
2024-03-04 14:04:27 -0800

    Second Commit on tank, add paint

---
+++ src/main/java/com/msb/TankFrame.java
@@ -0,0 +1,48 @@
+package com.msb;
+
+import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class TankFrame extends Frame {
+    int x = 200;
+    int y = 200;
+
+    public TankFrame() {
+        setSize(800, 600);
+        setResizable(false);
+        setTitle("tank war");
+        setVisible(true);
+        this.addKeyListener(new MyKeyListener());
+        addWindowListener(new WindowAdapter() { // the X on the window
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        System.out.println("paint");
+        g.fillRect(x, y, 50, 50);
+        x += 10;
+        y += 10;
+    }
+
+    class MyKeyListener extends KeyAdapter {
+        @Override
+        public void keyPressed(KeyEvent e) {
+            //x+=10;
+            //y+=10;
+            //repaint();
+        }
+
+        @Override
+        public void keyReleased(KeyEvent e) {
+            System.out.println("key Released");
+        }
+    }
+}

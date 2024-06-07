0c658fe4455103bbe18a94dc9a7a4da12fb1e61a
fguo2023 <fguo520@gmail.com>
2024-06-07 11:10:18 -0700

    continue check in

--- NettyStudy/src/main/java/com/msb/nettystudy/nio/PoolServer.java
+++ NettyStudy/src/main/java/com/msb/nettystudy/nio/PoolServer.java
@@ -1,5 +1,96 @@
 package com.msb.nettystudy.nio;
 
-public class PoolServer {
+import io.netty.channel.ServerChannel;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.Iterator;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class PoolServer {
+    ExecutorService pool = Executors.newFixedThreadPool(50);
+    private Selector selector;
+
+
+    public static void main(String[] args) throws IOException {
+        PoolServer server = new PoolServer();
+        server.initServer(8888);
+        server.listen(8888);
+    }
+
+    private void listen(int port) throws IOException {
+        while (true) {
+            selector.select();
+            Iterator ite = this.selector.selectedKeys().iterator();
+            while (ite.hasNext()) {
+                SelectionKey key = (SelectionKey) ite.next();
+                ite.remove();
+                if (key.isAcceptable()) {
+                    ServerSocketChannel server = (ServerSocketChannel) key.channel();
+                    SocketChannel channel = server.accept();
+                    channel.configureBlocking(false);
+                    channel.register(this.selector, SelectionKey.OP_READ);
+                } else if (key.isReadable()) {
+                    key.interestOps(key.interestOps() & (-SelectionKey.OP_READ));
+                    pool.execute(new ThreadHandlerChannelKey(key));
+                }
+            }
+        }
+    }
+
+    class ThreadHandlerChannelKey extends Thread {
+        private SelectionKey key;
+
+        public ThreadHandlerChannelKey(SelectionKey key) {
+            this.key = key;
+        }
+
+        @Override
+        public void run() {
+            SocketChannel channel = (SocketChannel) key.channel();
+            ByteBuffer buffer = ByteBuffer.allocate(1024);
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            int size = 0;
+            while (true) {
+                try {
+                    if (!((size = channel.read(buffer)) > 0)) {
+                        buffer.flip();
+                        baos.write(buffer.array(), 0, size);
+                        buffer.clear();
+                    }
+                    baos.close();
+                    byte[] content = baos.toByteArray();
+                    ByteBuffer writeBuf = ByteBuffer.allocate(content.length);
+                    writeBuf.put(content);
+                    writeBuf.flip();
+                    channel.write(writeBuf);
+                    if (size == -1) {
+                        channel.close();
+                    } else {
+                        key.interestOps(key.interestOps() | SelectionKey.OP_READ);
+                        key.selector().wakeup();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private void initServer(int portNumber) throws IOException {
+        ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
+        serverSocketChannel.configureBlocking(false);
+        serverSocketChannel.socket().bind(new InetSocketAddress(portNumber));
+        this.selector = Selector.open();
+        serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
+        System.out.println("server starting ...");
+    }
 }
--- NettyStudy/src/main/java/com/msb/nettystudy/nio/Server.java
+++ NettyStudy/src/main/java/com/msb/nettystudy/nio/Server.java
@@ -1,8 +1,11 @@
 package com.msb.nettystudy.nio;
 
 
+import io.netty.handler.codec.bytes.ByteArrayEncoder;
+
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
@@ -12,6 +15,7 @@ import java.util.Set;
 
 // nio has two ways to program: use one thread to do everything or use one thread + thread pool to do everything
 // below uses one thread, netty uses two thread pools
+// below uses only one thread
 public class Server {
     public static void main(String[] args) throws IOException {
         ServerSocketChannel ssc = ServerSocketChannel.open(); // channel 全双工
@@ -22,11 +26,11 @@ public class Server {
         Selector selector = Selector.open(); // 大管家 轮询
         ssc.register(selector, SelectionKey.OP_ACCEPT); // 这个事件是用来关心客户端连接的
 
-        while(true){
-            selector.select();
+        while (true) {
+            selector.select(); // 这个也是阻塞方法
             Set<SelectionKey> keys = selector.selectedKeys();
             Iterator<SelectionKey> it = keys.iterator();
-            while(it.hasNext()){
+            while (it.hasNext()) {
                 SelectionKey key = it.next();
                 it.remove();
                 handle(key);
@@ -37,13 +41,39 @@ public class Server {
     }
 
     private static void handle(SelectionKey key) {
-        if(key.isAcceptable()){
+        if (key.isAcceptable()) {
             try {
                 ServerSocketChannel ssc = (ServerSocketChannel) key.channel();
                 SocketChannel sc = ssc.accept();
                 sc.configureBlocking(false);
+                sc.register(key.selector(), SelectionKey.OP_READ);
             } catch (IOException e) {
                 throw new RuntimeException(e);
+            } finally {
+
+            }
+        } else if (key.isReadable()) {
+            SocketChannel sc = null;
+            try {
+                sc = (SocketChannel) key.channel();
+                ByteBuffer buffer = ByteBuffer.allocate(512); // ByteBuffer is hard to use (only one pointer), so netty use ByteBuf (2 pointers, write pointer and read pointer)
+                buffer.clear();
+                int len = sc.read(buffer);
+                if (len != -1) {
+                    System.out.println(new String(buffer.array(), 0, len));
+                }
+                ByteBuffer bufferToWrite = ByteBuffer.wrap("HelloClient".getBytes());
+                sc.write(bufferToWrite);
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (sc != null) {
+                    try {
+                        sc.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
             }
         }
     }
